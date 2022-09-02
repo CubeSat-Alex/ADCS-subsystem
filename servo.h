3c02bd286399329794e798a2c79d33d4c4f66bd3
Saleh Ahmed <89755662+salehafahmy@users.noreply.github.com>
2022-09-02 16:13:49 +0200

    initial commit

--- servo.h
+++ servo.h
@@ -1,70 +1,77 @@
-#include <ServoTimer2.h> 
+#include <ServoTimer2.h>
 #include "GYRO.h"
 ServoTimer2 servox;
 ServoTimer2 servoy;// create two object for each servo motor
 
 void servo_setup()
-{  Serial.println("servo intialized");
-    servox.attach(6); // configuration servo motors with PWM pins
-    servoy.attach(5);
+{ //
+  Serial.println("servo intialized");
+  servox.attach(6); // configuration servo motors with PWM pins
+  servoy.attach(5);
 }
 
 void servox_motion(int  curr_ang, int des_ang) //pass angle as int
 {
-    
-    if ((curr_ang-des_ang)<-5) //make tolerance
-    {
-        while((curr_ang<des_ang)&&(des_ang<120)&&(des_ang>-1))//maximum pos 120,minimum pos 0 and add negative condition for first reading
-        {  
-            servox.write(servox.read()+15);
-            delay(10);
-            
-            mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-           // Serial.print("curr_angX after motor: ");
-            gyrox=map(ax, -17000, 17000, -90, 90) ;
-            //Serial.println(curr_ang);
-             // EEPROM.write(0, gyrox);
-            curr_ang=map(servox.read(),750,2250,0,120);
-        }
+  curr_ang = map(servox.read(), 750, 2250, 0, 120);
+  if ((curr_ang - des_ang) < -5) //make tolerance
+  {Serial.println(des_ang);
+    while ( (curr_ang < des_ang) && (des_ang < 120) && (des_ang > -1)){
+      servox.write(servox.read() + 15);
+      delay(50);
+      
+      mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      Serial.print("curr_angX after motor: ");
+      Serial.println(curr_ang);
+       gyrox = map(ax, -17000, 17000, -90, 90) ;
+      // EEPROM.write(0, gyrox);
+      curr_ang = map(servox.read(), 750, 2250, 0, 120);
     }
-    else if ((curr_ang-des_ang)>5)
+  }
+  else if ((curr_ang - des_ang) > 5)
+  { Serial.println("else");
+    while ( (curr_ang > des_ang) && (des_ang < 120) && (des_ang> -1) )
     {
-        while((curr_ang>des_ang)|| (des_ang<120)||(des_ang<-1) )
-        {
-            servox.write(servox.read()-15);
-            delay(10);
-             mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-            gyrox=map(ax, -17000, 17000, -90, 90) ;
-            //EEPROM.write(0, gyrox);
-            curr_ang=map(servox.read(),750,2250,0,120);
-        }
-
+      servox.write(servox.read() - 15);
+      delay(50);
+      mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      Serial.print("curr_angX after motor: ");
+      Serial.println(curr_ang);
+      gyrox = map(ax, -17000, 17000, -90, 90) ;
+      //EEPROM.write(0, gyrox);
+      curr_ang = map(servox.read(), 750, 2250, 0, 120);
     }
- //   return gyrox;
+  }
+  //   return gyrox;
 }
+
 void servoy_motion(int  curr_ang, int des_ang) //pass angle as int
 {
-    if ((curr_ang-des_ang)<-5)
+  curr_ang = map(servoy.read(), 750, 2250, 0, 120);
+  if ((curr_ang - des_ang) < -5)
+  {
+    while ( (curr_ang <= des_ang) && (des_ang < 120) && (des_ang > -1) )
     {
-        while( (curr_ang<=des_ang)&&(des_ang<120)&&(des_ang>-1) )
-        {
-            servoy.write(servoy.read()+15);
-            delay(10);
-            mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-            gyroy=map(ay, -17000, 17000,-90, 90) ;
-            curr_ang=map(servoy.read(),750,2250,0,120);
-        }
+      servoy.write(servoy.read() + 15);
+      delay(50);
+      mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      Serial.print("curr_angY after motor: ");
+      Serial.println(curr_ang);
+      gyroy = map(ay, -17000, 17000, -90, 90) ;
+      curr_ang = map(servoy.read(), 750, 2250, 0, 120);
     }
-    else if ((curr_ang-des_ang)>5)
+  }
+  else if ((curr_ang - des_ang) > 5)
+  {
+    while ( (curr_ang > des_ang) && (des_ang < 120) && (des_ang > -1) )
     {
-        while( (curr_ang>des_ang)&&(des_ang<120)&&(des_ang>-1) )
-        {
-            servoy.write(servoy.read()-15);
-            delay(10);
-             mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-            gyroy=map(ay, -17000, 17000, -90, 90) ;
-            curr_ang=map(servoy.read(),750,2250,0,120);
-        }
+      servoy.write(servoy.read() - 15);
+      delay(50);
+      mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      Serial.print("curr_angY after motor: ");
+      Serial.println(curr_ang);
+      gyroy = map(ay, -17000, 17000, -90, 90) ;
+      curr_ang = map(servoy.read(), 750, 2250, 0, 120);
+    }
 
-    }
+  }
 }
